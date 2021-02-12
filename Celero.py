509fdf6226bda3ea7d1d71772f5a32518f02f213
leoprasel <59002568+leoprasel@users.noreply.github.com>
2021-02-12 16:56:47 -0300

    started readme docummentation

--- Celero.py
+++ Celero.py
@@ -8,33 +8,41 @@ import re
 import pickle
 import nltk
 nltk.download('wordnet', quiet=True) 
-from nltk.stem.wordnet import WordNetLemmatizer
-from sklearn.feature_extraction.text import CountVectorizer
+#from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+#from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
-#Python CLI options
+#Python CLI options configuration
 @click.command()
 @click.option('--modo', '-m')
 @click.option('--diretorio', '-d')
 
 def main(modo, diretorio):
     #Function to clean the text, replacing HTML tags, punctuations and other undesirable characters
-    #This function will also get the root of the word in order to get a better accuracy(Lemmatization)
+    #This function will also get the root of the word in order to get a better accuracy
     def cleaning(text):
         text = re.sub(r'<.*?>', '', text)
+        text = re.sub("n't",' not', text)
         text = re.sub('[^a-zA-Z]',' ', text)
         text = text.strip().lower().split()
         
-        lmtzr = WordNetLemmatizer()
-        text = [lmtzr.lemmatize(word) for word in text]
+        ps = PorterStemmer()
+        text = [ps.stem(word) for word in text]
+
+        #An alternative is to use a word Lemmatizer instead of the porterstemmer:
+        #The results were supposed to be better, but in my tests the porterstemmer did better.
+        #lmtzr = WordNetLemmatizer()
+        #text = [lmtzr.lemmatize(word) for word in text]
+        
         text = ' '.join(text)
         return text
     
     if modo=='treino':
-        
-        #Importing training mode exclusive libraries for performance
-        
         print('Started at: ',datetime.now())
+        time_start = datetime.now()
 
+        #Importing training mode exclusive libraries for performance
         from sklearn.svm import LinearSVC
         from sklearn.metrics import confusion_matrix
         from sklearn.metrics import classification_report
@@ -45,8 +53,6 @@ def main(modo, diretorio):
         train_path_pos = diretorio +  '/train/pos//'
         test_path_neg = diretorio + '/test/neg//'
         test_path_pos = diretorio + '/test/pos//'
-
-        
 
         #Function to read the text files and concatenate them into a pandas dataframe
         def read_files(files_path, evaluation, mode):
@@ -69,25 +75,34 @@ def main(modo, diretorio):
         test_pos_df = read_files(test_path_pos, 1, 'test')
         test_df = pd.concat([test_neg_df, test_pos_df]).reset_index(drop=True)
         
+        time_read = datetime.now()
+        print('Reading files took: ', (time_read - time_start).total_seconds(), 'seconds')
+
         #Cleaning the data
         print('Cleaning files...')
         train_df['cleaned_text'] = train_df.text.apply(lambda x: cleaning(x))
         test_df['cleaned_text'] = test_df.text.apply(lambda x: cleaning(x))
 
         #Creating the Bag of Words vector
-        vectorizer = CountVectorizer(stop_words="english", max_features=500)
+        vectorizer = TfidfVectorizer(stop_words="english", max_features=2000)
 
-        training_features = vectorizer.fit_transform(train_df["cleaned_text"])
-        test_features = vectorizer.transform(test_df["cleaned_text"])
+        train_bow_vector = vectorizer.fit_transform(train_df["cleaned_text"])
+        test_bow_vector = vectorizer.transform(test_df["cleaned_text"])
+
+        time_clean = datetime.now()
+        print('Cleaning files took: ', (time_clean - time_read).total_seconds(), 'seconds')
 
         # Training the support vector classifier
         print('Training model...')
         model = LinearSVC()
-        model.fit(training_features, train_df["evaluation"])
-        y_pred = model.predict(test_features)
+        model.fit(train_bow_vector, train_df["evaluation"])
+        pred = model.predict(test_bow_vector)
+
+        time_model = datetime.now()
+        print('Traning the model took: ', (time_model - time_clean).total_seconds(), 'seconds')
 
         # Results
-        accuracy = accuracy_score(test_df["evaluation"], y_pred)
+        accuracy = accuracy_score(test_df["evaluation"], pred)
 
         print("Precisao do modelo: {:.2f}".format(accuracy*100))
 
@@ -99,13 +114,14 @@ def main(modo, diretorio):
 
         
         print('Ended at: ',datetime.now())
-    
+        time_end = datetime.now()
+        print('The entire program took: ', (time_end - time_start).total_seconds(), 'seconds')
+
     elif modo == 'execucao':
         with open('celero_model.pickle','rb') as file:
             model = pickle.load(file)
         with open('celero_vectorizer.pickle','rb') as file:
             vectorizer = pickle.load(file)
-
 
         review_list = []
         review = open(diretorio,'r', encoding='utf8').read()
@@ -115,8 +131,8 @@ def main(modo, diretorio):
 
         review_vec = vectorizer.transform(df['text'])
 
-        y_pred = model.predict(review_vec)
-        if int(y_pred) == 1:
+        pred = model.predict(review_vec)
+        if int(pred) == 1:
             print('Positive Review!')
         else:
             print('Negative Review!')
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Celero
-Teste para desenvolvedor de AI
+## Desafio para desenvolvedor de AI
+### Objetivo:
+Neste projeto utilizaremos uma base de dados de comentários de filmes feitos no site de reviews IMDB dispobilizado pela [universidade de Stanford](https://ai.stanford.edu/~amaas/data/sentiment/).
+
+O objetivo do desafio é treinar uma inteligência artificial que possui dois modos: 
+* O modo treino, no qual ela irá ser treinada com o dataset acima, sendo inputado o diretório da pasta do dataset de treino.
+* E o modo execução, onde ao ser inputado um arquivo texto com uma review de um filme, a AI deve retornar se este comentário é um comentário de cunho positivo ou negativo.
+
+### Considerações iniciais:
+* O problema se trata de uma inteligência artificial NLP (Natural Language Processing). Para AI's deste tipo, tanto Machine Learning como Redes neurais são possíveis.
+  * Em datasets maiores, redes neurais performam melhor, porém estas possuem uma comlpexidade muito maior para serem desenvolvidas. Portanto, iremos utilizar machine learning.
+* Como os algoritmos de machine learning de NLP transformam as frases em vetores, é ideal um algoritmo de classificação que seja amigável com um numero grande de features, como o Naive Bayes ou Support Vector Machines.
+
+### Vetorização de palavras:
+Para montar o modelo, as frases são inputadas por meio de um vetor de palavras, com todas as palavras do dataset ocupando uma posição do vetor.
+#### max_features:
+Quanto mais palavras existem nesse vetor, mais complexo é o modelo, aumentando a probabilidade de bias no modelo. Irrestrito, o modelo chega a mais de 70 mil palavras diferentes, sendo que existem 171 mil palavras no dicionário inglês.
+
+Estima-se que 3 mil palavras encobrem 95% das palavras escritas coloquialmente.
+
+
+### Naive Bayes vs SVC:
+#### SVC:
